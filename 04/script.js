b37042f754f1a988a0aa20bd3fa4e541251ac85e
aincatoni <ain.catoni@gmail.com>
2019-01-14 09:27:58 -0300

    completando ejercicio 4

--- 04/script.js
+++ 04/script.js
@@ -2,11 +2,13 @@ $(function(){
 
 $('.add_name').on('click', function(e) {
 var addName = prompt('por favor agrega tu nombre')
+  $('.names').prepend('<li><span>' + addName + '</span>' + '<a href="#" class="remove_name">Eliminar Nombre</a>' + '</li>');
   e.preventDefault();
-  $('.names').prepend("<li>"+addName+"</li>");
  });
 
-
+ $('body').on('click', 'a', function () {
+  $(this).parent().remove();
+})
 
 
 });
